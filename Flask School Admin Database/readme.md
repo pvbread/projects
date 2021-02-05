f61a425499b59577a5144f359e3e3d6eafb007c5
pvbread <64717180+pvbread@users.noreply.github.com>
2021-02-05 14:58:24 -0500

    Delete Flask School Admin Database directory

--- Flask School Admin Database/readme.md
+++
@@ -1,8 +0,0 @@
-Work in progress - Flask Framework
-
-<br />
-<br />
-December 24, 2020
-<br />
-
-This is going to be an online admin system for basic administration of student/teacher roster and will also take care of scheduling.
--- Flask School Admin Database/static/jpi.js
+++
@@ -1,9 +0,0 @@
-//**********jpi form master
-let newView = document.getElementById("view-button");
-let newViewSelect = document.getElementById("select-view");
- 
-newView.onclick = function(){
-	
-    document.getElementsByClassName("class-name")[2].cells[3].textContent = "hi";
-
-}
